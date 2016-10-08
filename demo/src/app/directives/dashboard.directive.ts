2897f3763f13709d684d87e421f0c005df4dffd6
jaumard <jimmy.aumard@gmail.com>
2016-10-08 17:30:49 +0200

    Initial widget position

--- demo/src/app/directives/dashboard.directive.ts
+++ demo/src/app/directives/dashboard.directive.ts
@@ -1,18 +1,14 @@
-import {EventEmitter, ContentChildren, Directive, Output, OnInit, AfterViewInit, Renderer, ElementRef, QueryList} from '@angular/core';
+import {EventEmitter, ContentChildren, Directive, Input, Output, OnInit, AfterViewInit, Renderer, ElementRef, QueryList} from '@angular/core';
 import {Widget} from "./widget.directive";
 
 @Directive({
   selector: '[dashboard]',
-  host: { //TODO this should be done on all child not here
-    '(mousedown)': '_onMouseDown($event)',
-    '(mousemove)': '_onMouseMove($event)',
-    '(mouseup)': '_onMouseUp($event)',
-    '(touchstart)': '_onMouseDown($event)',
-    '(touchmove)': '_onMouseMove($event)',
-    '(touchend)': '_onMouseUp($event)',
+  host: {
     '(window:resize)': '_onResize($event)',
     '(document:mousemove)': '_onMouseMove($event)',
-    '(document:mouseup)': '_onMouseUp($event)'
+    '(document:mouseup)': '_onMouseUp($event)',
+    '(document:touchmove)': '_onMouseMove($event)',
+    '(document:touchend)': '_onMouseUp($event)'
   }
 })
 export class Dashboard implements OnInit, AfterViewInit {
@@ -21,11 +17,14 @@ export class Dashboard implements OnInit, AfterViewInit {
   @Output() public onDrag: EventEmitter<Widget> = new EventEmitter<Widget>();
   @Output() public onDragStop: EventEmitter<Widget> = new EventEmitter<Widget>();
 
+  @Input() margin: number = 10;
+  @Input() handle: string;
+
   //	Public variables
   public dragEnable: boolean = true;
 
   //	Private variables
-  //private _items: Array<Widget> = [];
+  private _width: number = 0;
   private _destroyed: boolean = false;
   private _isDragging: boolean = false;
   private _dragReady: boolean = false;
@@ -40,7 +39,12 @@ export class Dashboard implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    console.log(this._items);
+    this._width = this._ngEl.nativeElement.offsetWidth;
+    console.log(this._ngEl.nativeElement.offsetWidth);
+    this._items.forEach(item => {
+      item.setEventListener(this.handle, this._onMouseDown.bind(this));
+    })
+    this._calculPositions();
   }
 
   public enableDrag(): void {
@@ -59,23 +63,49 @@ export class Dashboard implements OnInit, AfterViewInit {
 
   }
 
+  private _calculPositions(): void {
+    let top = this.margin;
+    let left = this.margin;
+
+    let items = this._items.toArray();
+    items[0].setPosition(top, left);
+
+    for (let i = 1; i < items.length; i++) {
+      let item = items[i];
+      if((left + item.width*2 + this.margin) < this._width) {
+        left +=  item.width + this.margin;
+      }
+      else {
+        left = this.margin;
+        top += item.height + this.margin;
+      }
+      item.setPosition(top, left);
+    }
+
+  }
 
   private _onResize(e: any): void {
     console.log('_onResize');
+    this._width = this._ngEl.nativeElement.offsetWidth;
+    this._calculPositions();
   }
 
   private _onMouseDown(e: any): boolean {
     console.log('_onMouseDown');
+    this._isDragging = true;
     return true;
   }
 
   private _onMouseMove(e: any): boolean {
-    console.log('_onMouseMove');
+    if(this._isDragging) {
+      console.log('_onMouseMove');
+    }
     return true;
   }
 
   private _onMouseUp(e: any): boolean {
     console.log('_onMouseUp');
+    this._isDragging = false;
     return true;
   }
 
--- demo/src/app/directives/widget.directive.ts
+++ demo/src/app/directives/widget.directive.ts
@@ -3,13 +3,36 @@ import {Directive, OnInit, Renderer, ElementRef} from '@angular/core';
 @Directive({
   selector: '[widget]'
 })
-export class Widget implements OnInit{
+export class Widget implements OnInit {
 
   constructor(private _ngEl: ElementRef,
-              private _renderer: Renderer) { }
+              private _renderer: Renderer) {
+  }
 
   ngOnInit(): void {
     this._renderer.setElementClass(this._ngEl.nativeElement, 'widget', true);
   }
 
+  public get width() {
+    return this._ngEl.nativeElement.offsetWidth;
+  }
+
+  public get height() {
+    return this._ngEl.nativeElement.offsetHeight;
+  }
+
+  public setPosition(top: number, left: number): void {
+    this._renderer.setElementStyle(this._ngEl.nativeElement, 'top', top + 'px');
+    this._renderer.setElementStyle(this._ngEl.nativeElement, 'left', left + 'px');
+  }
+
+  public setEventListener(handle: string, cb: Function): void {
+    if (handle) {
+
+    }
+    else {
+      this._renderer.listen(this._ngEl.nativeElement, 'mousedown', cb);
+      this._renderer.listen(this._ngEl.nativeElement, 'touchstart', cb);
+    }
+  }
 }
